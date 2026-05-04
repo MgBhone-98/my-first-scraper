efbe9df7fbf8a7c02500fd9347ece87378d00a1c
mgbhone <mgwinhtutbhone@gmail.com>
2026-05-04 22:27:42 +0630

    Fix timezone Asia/Yangon

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 import datetime
+import pytz
+
 
 def get_currency():
     # Free API သုံးပြီး ငွေလဲနှုန်းယူမယ်
@@ -11,10 +13,15 @@ def get_currency():
 
         if data['result'] == 'success':
             mmk_rate = data['rates']['MMK']
-            now = datetime.datetime.now()
+
+            #now = datetime.datetime.now()
+            # မြန်မာစံတော်ချိန် သတ်မှတ်ခြင်း
+            tz_MM = pytz.timezone('Asia/Yangon')
+            now_MM = datetime.datetime.now(tz_MM)
 
             print(f"--- Exchange Rate Update ---")
-            print(f"Date: {now.strftime('%Y-%m-%d %H:%M:%S')}")
+            print(f"Date: {now_MM.strftime('%Y-%m-%d %H:%M:%S')}")
+            print(f"1 USD = {mmk_rate} MMK")  # ဒီစာကြောင်း ပါမှ MMK ထွက်မှာပါ
             print(f"----------------------------")
         else:
             print("Error: Could not fetch data.")
